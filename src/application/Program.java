2947cdd781c39b9fb091f50d5cfe187e11b8fef8
Eduardo Pereira Filho <eduardopereirafilho6@gmail.com>
2022-10-19 10:26:00 -0300

    Reference method with static method

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 
         Double min = 100.0;
 
-        list.forEach(new PriceUpdate());
+        list.forEach(Product::staticProductConsumer);
         list.forEach(System.out::println);
 
     }
